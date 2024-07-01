cdfece0b7c5558d97b301e371f8fb75620eae7a1
Tmwonder1 <fabiandname@gmail.com>
2024-07-01 18:17:50 +0100

    cvv

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+
+# install flask from pip3 using puppet
+
+$pkg_name = 'flask'
+
+package { $pkg_name:
   ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
